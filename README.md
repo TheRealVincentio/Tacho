31916d043604f7caf85a0e90655c257d6e45354e
mperina11 <mperina173@gmail.com>
2022-10-12 12:37:58 -0700

    numbers, shifts, points

--- README.md
+++ README.md
@@ -2,10 +2,5 @@
 
 Change gears and disappear ... with the occasional stop sign
 
-To Do
-- [ ] Numbers on Dial
-- [ ] Shift
-- [ ] Stop
-- [ ] Points
 
 ## [FIX ME]()
--- Tacho/main.js
+++ Tacho/main.js
@@ -6,33 +6,110 @@ Shift and Stop!
 
 characters = [
 `
- l l l 
-lllllll
-ll l ll
-ll l ll
-lllllll
-ll   ll
-ll   ll
+  ll
+ll  ll
+ll  ll
+  ll
 `,`
+ ll
+ ll
+ ll
+ ll
+`,`
+ll
+  ll
+ll
+llll
+`,`
+ll
+  ll
+ll
+  ll
+ll
+`,`
+ll ll
 lllll
-lllll
-lllll
+   ll
+`,`
+llll
+ll
+  ll
+ll
+`,`
+  ll
+ll
+l l
+l  ll
+llll
+`,`
+llllll
+    ll
+  ll
+ll
+ll
+`,`
+  ll
+ll  ll
+  ll
+ll  ll
+  ll
+`,`
+  ll
+ll  ll
+ll  ll
+  ll
+ll
+ll
+`,`
+ll    ll
+ll  ll  ll
+ll  ll  ll
+ll    ll
+`,`
+ll  ll
+ll  ll
+ll  ll
+`,`
+ll  ll
+ll    ll
+ll  ll
+ll  llll
+`,`
+ll  ll
+ll    ll
+ll  ll
+ll    ll
+ll  ll
+`,`
+ll  ll ll
+ll  lllll
+ll     ll
+`,`
+ll  llll
+ll  ll
+ll    ll
+ll  ll
 `
 ];
 
 const G = {
-	WIDTH: 100,
+	WIDTH: 110,
 	HEIGHT: 100,
-
 }
 
-const COLORS = ["red", "blue", "yellow", "purple"];
-
-let index = 0;
-
-// JSDocs
-
-
+const tacX = G.WIDTH/2;
+const tacY = G.HEIGHT/1.5;
+const r_blue_red = 50;
+const r_num = 40;
+const characts = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q"];
+let points_blue = [];
+let points_red = [];
+let points_num = [];
+let needle_index = 0;
+let redline_time = 0;
+let points_bckrnd = [];
+let r_bckrnd = 55;
+// let engine_broken = false;
 
 options = {
 	viewSize: {x: G.WIDTH, y: G.HEIGHT},
@@ -47,65 +124,145 @@ options = {
 
 function update() {
 	if (!ticks) {
-        
-	}
+        // points for numbers
+        for (let angle = 160; angle <= 380; angle += 14.66) {
 
-    let tacX = G.WIDTH/2;
-    let tacY = G.HEIGHT/1.2;
-    let radius = 50;
-    // color("red");
-    // arc(tacX, tacY, 45, 2);
+            let rad = (angle * Math.PI)/180;
+            let x = tacX + (r_num * cos(rad));
+            let y = tacY + (r_num * sin(rad));
+            points_num.push({
+                x: x,
+                y: y
+            })
+        }
+        // points for blue arc
+        for (let angle = 160; angle <= 380; angle += 1) {
+            
+            let rad = (angle * Math.PI)/180;
+            let x = tacX + (r_blue_red * cos(rad));
+            let y = tacY + (r_blue_red * sin(rad));
 
-    color("blue");
-    rect(0, G.HEIGHT/1.2, radius*2, 20);
+            points_blue.push({
+                x: x,
+                y: y
+            });            
+        }
+        // Draw Red arc
+        for (let angle = 320; angle <= 380; angle += 1) {
+            let rad = (angle * Math.PI)/180;
+            let x = tacX + (r_blue_red * cos(rad));
+            let y = tacY + (r_blue_red * sin(rad));
+            
+            points_red.push({
+                x: x,
+                y: y
+            })
+        }
+        // Draw background arc
+        for (let angle = 160; angle <= 380; angle += 1) {
+            let rad = (angle * Math.PI)/180;
+            let x = tacX + (r_bckrnd * cos(rad));
+            let y = tacY + (r_bckrnd * sin(rad));
+            
+            points_bckrnd.push({
+                x: x,
+                y: y
+            })
+        }
+	}   
 
-    // 270 - 90 = 180
+    // draw numbers
+    for (let i = 0; i < points_num.length; i++) {        
+        color("blue");
+        if (i >= 11) {
+            color("red");
+        }
+        char(characts[i], points_num[i].x, points_num[i].y);
+     }
 
-    let points = [];
-
-    for (let angle = 180; angle <= 360; angle += 1) {
-        
-        let rad = (angle * Math.PI)/180;
-        let x = tacX + (radius * cos(rad));
-        let y = tacY + (radius * sin(rad));
-
-        points.push({
-            x: x,
-            y: y
-        });
-
-        // console.log("x: ", x, " y: ", y, " a: ", angle, " rad: ", rad);
-        // console.log(points[50]);
-
+    // draw blue arc
+    for (let i = 0; i < points_blue.length; i++) {
         color("cyan");
-        rect(x, y, 2, 2);
+        rect(points_blue[i].x, points_blue[i].y, 2, 2);
     }
 
-    for (let angle = 310; angle <= 360; angle += 1) {
-        let rad = (angle * Math.PI)/180;
-        let x = tacX + (radius * cos(rad));
-        let y = tacY + (radius * sin(rad));
+    // draw red arc
+    for (let i = 0; i < points_red.length; i++) {
         color("red");
-        rect(x, y, 2, 2);
+        rect(points_red[i].x, points_red[i].y, 2, 2);
     }
-
-    // for (let i=0; i < points.length; i += 15) {
-
-    // }
-    
-    if (ticks % 30 == 0) {
-        console.log("Now");
-        console.log(points[index]);
-        index += 3;
-
-        if (index >= points.length) {
-            index = 0; // loop back to start
+    // draw bckrnd arc
+    for (let i = 0; i < points_bckrnd.length; i++) {
+        color("blue");
+        rect(points_bckrnd[i].x, points_bckrnd[i].y, 2, 2);
+    }
+   
+    // Move needle
+    if (ticks % 30 == 0) { 
+        if (needle_index <= 90) {
+            needle_index += 5;
+        } 
+        else if (needle_index <= 130) {
+            needle_index += 4;
+        }
+        else {
+            needle_index += 2;
+        }
+        
+        if (needle_index >= points_blue.length) {
+            needle_index = 0; // loop back to start
         }
     }
+    // draw needle
     color("green");
-    line(tacX, tacY, points[index].x, points[index].y, 3);
+    line(tacX, tacY, points_blue[needle_index].x, points_blue[needle_index].y, 3);
 
-    // color("green");
-    // line(tacX, tacY, points[50].x, points[50].y, 3);
+    // Box
+    // color("blue");
+    // rect(0, G.HEIGHT/1.2, radius*2, 20);
+
+    // Shift
+    if (input.isJustPressed) {
+        play("powerUp");
+        if (needle_index <= 40) { // 3:42,  6:90, 9:130, 12:170, RED:165+
+            needle_index = Math.floor(Math.random() * (20 - 1) + 1);
+            addScore(3);          
+        }
+        else if (needle_index <= 90) {
+            needle_index = Math.floor(Math.random() * (40 - 20) + 20);
+            addScore(3);
+        }
+        else if (needle_index <= 130) {
+            needle_index = Math.floor(Math.random() * (65 - 40) + 40);
+            addScore(3);
+        }
+        else if (needle_index <= 160) {
+            needle_index = Math.floor(Math.random() * (80 - 65) + 65);
+            addScore(3);
+        }
+        else {
+            needle_index = Math.floor(Math.random() * (100 - 65) + 65);
+            addScore(1);
+        }
+    }
+
+    // Redline
+    if (needle_index >= 160) {
+        if (ticks % 60 == 0) { // add one sec
+            redline_time++;
+            play("explosion");
+            console.log("redline_time: ", redline_time);
+        }
+    } else {
+        redline_time = 0;
+        console.log("redline_time zeroed");
+    }
+
+    // Redline gameover
+    if (redline_time >= 3) {
+        console.log("END");
+        play("explosion");
+        end();
+    }   
 
 }
